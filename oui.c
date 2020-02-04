8e72cdad548ed072a590b1a98107a83041d1aa1e
DingerM GD1-2 <32112541+Sthaim@users.noreply.github.com>
2020-02-04 22:21:16 +0100

    Final Partie Achat marchand

--- oui.c
+++ oui.c
@@ -10,7 +10,7 @@ struct Objet
 
 typedef struct Objet objet;
 
-void triTableau(objet *tableau, int * index){
+void triTableauInv(objet *tableau, int * index){
   objet trade;
 	int counter;
 	while (counter!=*index){
@@ -20,7 +20,6 @@ void triTableau(objet *tableau, int * index){
 	      trade=tableau[i];
 	      tableau[i]=tableau[i+1];
 	      tableau[i+1]=trade;
-				printf("tri entre : %d et %d\n",i,i+1);
 	    }
 			else{
 				counter++;
@@ -29,30 +28,83 @@ void triTableau(objet *tableau, int * index){
 	}
 }
 
-void ajoutTableau(objet * thing, int * index, objet *tableau){
-  tableau[*index]=*thing;
-  *index=*index+1;
-	triTableau(tableau,index);
+void triTableauMar(objet *tableau, int * index){
+  objet trade;
+	int counter;
+	while (counter!=*index){
+		counter=1;
+	  for (int i=0; i<*index-1;i++){
+	    if (tableau[i].prix<tableau[i+1].prix){
+	      trade=tableau[i];
+	      tableau[i]=tableau[i+1];
+	      tableau[i+1]=trade;
+	    }
+			else{
+				counter++;
+			}
+	  }
+	}
 }
 
+void ajoutTableauInv(objet * thing, int * index, objet *tableau){
+  tableau[*index]=*thing;
+  *index=*index+1;
+	triTableauInv(tableau,index);
+}
 
-void afficherTableau(objet *tableau, int index){
+void ajoutTableauMar(objet * thing, int * index, objet *tableau){
+  tableau[*index]=*thing;
+  *index=*index+1;
+	triTableauMar(tableau,index);
+}
+
+void enlever(int * choix, int * index, objet *tableau){
+  for(int i=*choix;i<*index-1;i++){
+		tableau[i]=tableau[i+1];
+	}
+	*index-=1;
+}
+
+void afficherTableauInv(objet *tableau, int index){
+	printf("Voici le contenu de votre inventaire:\n");
   for (int i=0; i<=index-1;i++){
     printf("Nom: %s  Prix: %d\n",tableau[i].nom,tableau[i].prix);
   }
 }
 
+void afficherTableauMar(objet *tableau, int index){
+	printf("Voici le contenu des produits du marchant:\n");
+  for (int i=0; i<=index-1;i++){
+    printf("%d: Nom: %s  Prix: %d\n",i+1,tableau[i].nom,tableau[i].prix);
+  }
+}
+
 int main(){
-  int index=0;
+  int indexI=0;
+	int indexM=0;
+	int choix;
   objet inventaire[20];
+	objet marchant[20];
   objet tomate={"tomate",2};
-  ajoutTableau(&tomate,&index,inventaire);
+  ajoutTableauInv(&tomate,&indexI,inventaire);
   objet yoyo={"yoyo",10};
-  ajoutTableau(&yoyo,&index,inventaire);
+  ajoutTableauInv(&yoyo,&indexI,inventaire);
   objet orange={"orange",20};
-  ajoutTableau(&orange,&index,inventaire);
+  ajoutTableauInv(&orange,&indexI,inventaire);
+	objet epeeInfinite={"epee d'infinite",1};
+	ajoutTableauMar(&epeeInfinite,&indexM,marchant);
+	objet onglePied={"ongle de pied",10000};
+	ajoutTableauMar(&onglePied,&indexM,marchant);
+	objet photoPoivron={"photo de poivron",5000};
+	ajoutTableauMar(&photoPoivron,&indexM,marchant);
 	for(int i=0;i<3;i++){
-		
+		afficherTableauInv(inventaire, indexI);
+		afficherTableauMar(marchant, indexM);
+		printf("Choisissez l'item de votre choix:\n");
+		scanf("%d",&choix);
+		ajoutTableauInv(&marchant[choix-1],&indexI,inventaire);
+		enlever(&choix,&indexM,marchant);
+		printf("indexM %d\n",indexM);
 	}
 	return 0;
 }
